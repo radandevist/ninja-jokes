edb971bcc21a0158fd7dee97955f5002834495d9
radan maerloa <radandevist@gmail.com>
2021-02-01 12:17:26 +0300

    home image

--- views/contents/home.html.php
+++ views/contents/home.html.php
@@ -1,6 +1,7 @@
 <div id="home">
     <div class='text-group text-center'>
-        <p class="text h3">Welcome to Internet Joke DataBase,<br>A collection of great jokes.</p>
-        <p class="small">Actually, up to <span><?php echo htmlspecialchars($totaljoke, ENT_QUOTES, 'utf-8') ?></span> jokes availlables in the database. </p>
+        <img class="home-image" src="/assets/images/ninja.png" alt="shinobi_image" srcset="">
+        <p class="text h3">Welcome to Ninja jokes,<br>A collection of jokes.</p>
+        <p class="">Actually, up to <span><?php echo htmlspecialchars($totaljoke, ENT_QUOTES, 'utf-8') ?></span> jokes availlables in the database. </p>
     </div>
 </div>
